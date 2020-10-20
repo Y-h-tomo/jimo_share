a431e9d5526b510fe65cc2e16a822c737a028f93
y_h_tomo <ruge.e.azul@gmail.com>
2020-10-21 00:06:19 +0900

    posts form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :content, {presence: true, length: {maximum: 140}}
+  validates :content, {presence: true, length: {maximum: 200}}
   validates :user_id, {presence: true}
   validates :image, {presence: true}
   validates :price, {presence: true, numericality: true}
