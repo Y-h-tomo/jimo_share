1c353ad31484b20a9fc1a874ea26169262ee2059
y_h_tomo <ruge.e.azul@gmail.com>
2020-10-24 16:01:41 +0900

    ユーザー一覧および詳細作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
   end
 
   def show
     @user = User.find_by(id: params[:id])
     @post = Post.where(user_id: @user.id)
-
   end
 
   def new
